f3119a3fc66a1f668904820377bf875fa5579794
Sobale123 <129029038+Sobale123@users.noreply.github.com>
2024-09-09 20:47:05 +0530

    Assignment 03

---
+++ Assignment03/script.js
@@ -0,0 +1,8 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const addToCartButton = document.getElementById('add-to-cart');
+    addToCartButton.addEventListener('click', () => {
+        alert('Product added to cart!');
+    });
+});
